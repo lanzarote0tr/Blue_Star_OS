9efa350c200cdf38fbc464e52f7cf48cd7552f43
hongii <ierwarf@gmail.com>
2025-12-12 18:27:14 +0900

    edk2 GUI multitasking

---
+++ run.sh
@@ -0,0 +1,13 @@
+. edksetup.sh
+build --platform=OvmfPkg/OvmfPkgX64.dsc --arch=X64 --buildtarget=RELEASE --tagname=GCC5
+
+cp Build/OvmfX64/RELEASE_GCC5/X64/BlueStarOS.efi EFIfile/os.efi
+
+qemu-system-x86_64 \
+    -bios OVMF.fd \
+    -drive if=pflash,format=raw,readonly=on,file=OVMF.fd \
+    -drive file=fat:rw:EFIfile,format=raw \
+    -net none \
+    -m 512M \
+    -monitor stdio \
+    -d int -D log.txt
