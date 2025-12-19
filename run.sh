aafbef600fed18300924bc84e0adfda66fc111e9
Lanzarote <realethanwoncho@gmail.com>
2025-12-19 13:05:37 +0900

    add ensure_base_tools()

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 set -e
+
+ensure_base_tools() {
+    # Build BaseTools C binaries if core tool (GenFw) is missing.
+    if [ ! -x BaseTools/Source/C/bin/GenFw ]; then
+        echo "Building BaseTools (missing GenFw)..."
+        make -s -C BaseTools/Source/C >/dev/null
+    fi
+}
+
+ensure_base_tools
+
 . edksetup.sh
 build --platform=OvmfPkg/OvmfPkgX64.dsc --arch=X64 --buildtarget=RELEASE --tagname=GCC5
 
